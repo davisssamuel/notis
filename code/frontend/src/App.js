084ac470c93f72684644b112c1309829f413124b
Notis Team <joshualees@cedarville.edu>
2023-09-25 18:34:41 -0400

    more logo

--- code/frontend/src/App.js
+++ code/frontend/src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+import logo from './logo.png';
 import './App.css';
 
 function App() {
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hello from notis VM!
         </p>
         <a
           className="App-link"
