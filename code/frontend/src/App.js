9fb89d880fd49e996c41e4da6fe0e7b6d4cbbaa4
Notis Team <joshualees@cedarville.edu>
2023-10-02 19:29:50 -0400

    simple device detection works

--- code/frontend/src/App.js
+++ code/frontend/src/App.js
@@ -1,25 +1,49 @@
 import logo from './logo.png';
 import './App.css';
+import { isMobile } from 'react-device-detect';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello from notis VM!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    if (isMobile) {
+        return (
+            <div className="App">
+              <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <p>
+                  You're on mobile! Yay!
+                </p>
+                <a
+                  className="App-link"
+                  href="https://reactjs.org"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Learn React
+                </a>
+              </header>
+            </div>
+          );
+    }
+    else {
+        return (
+            <div className="App">
+              <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <p>
+                  You're not on a mobile device.. sad :(
+                </p>
+                <a
+                  className="App-link"
+                  href="https://reactjs.org"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Learn React
+                </a>
+              </header>
+            </div>
+          );
+    }
+  
 }
 
 export default App;
