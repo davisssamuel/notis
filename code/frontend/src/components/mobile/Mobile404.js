bb66b239a90d4b3b48a7070abf2a80b8787fac52
davisssamuel <davissamuelg1@gmail.com>
2023-10-25 23:19:08 -0400

    began work with React Native

--- code/frontend/src/components/mobile/Mobile404.js
+++
@@ -1,9 +0,0 @@
-function Mobile404() {
-    return (
-        <>
-            <h1>404: not found</h1>
-        </>
-    );
-}
-
-export default Mobile404;
--- code/scripts/run.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/bash
-source ${HOME}/.bashrc
-
-cd /home/notis/Code/notis/code/frontend
-export NODE_ENV=development
-bun start
