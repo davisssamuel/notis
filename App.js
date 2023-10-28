c68325b759f2c5b0077a96dc100d36c2c7274d09
davisssamuel <davissamuelg1@gmail.com>
2023-10-28 02:05:12 -0400

    RNUI testing

--- App.js
+++ App.js
@@ -1,25 +1,29 @@
 import { StatusBar } from "expo-status-bar";
 import { ScrollView, StyleSheet } from "react-native";
-import { View, Text } from "react-native-ui-lib"; // this line breaks things... 
-import { TextInput } from "react-native";
+// import { View, Text, TextInput } from "react-native";
+// import { TextInput } from "react-native";
+import index from "react-native-ui-lib/src/style/index";
+import View from "react-native-ui-lib/view";
+import Text from "react-native-ui-lib/text";
+import TextField from "react-native-ui-lib/textField";
 import jsonData from "./data/chats.json";
 
 // creates list of chats in RN
 let chats = jsonData.map((jsonData) => {
   return (
     <View
-      style={{
-        // flex: 1,
-        width: "100%",
-        backgroundColor: "#eee",
-
-        // debug border
-        borderColor: "red",
-        borderWidth: 1,
-      }}
+      // style={{
+      //   // flex: 1,
+      //   width: "100%",
+      //   backgroundColor: "#eee",
+      //   // debug border
+      //   borderColor: "red",
+      //   borderWidth: 1,
+      // }}
+      style={index}
       key={jsonData.id}
     >
-      <Text> {jsonData.name} </Text>
+      <Text blue30> {jsonData.name} </Text>
       <Text> {jsonData.lastMessage} </Text>
     </View>
   );
@@ -27,22 +31,8 @@ let chats = jsonData.map((jsonData) => {
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <TextInput
-        style={{
-          backgroundColor: "#eee",
-        }}
-        defaultValue="Search"
-      />
-
-      {/* <MaskedInput
-        ref={inputRef}
-        renderMaskedText={
-          <Text text80 grey60>
-            {value}$
-          </Text>
-        }
-      /> */}
+    <View style={index}>
+      <TextField placeholder={"Search"} />
       <ScrollView>{chats}</ScrollView>
     </View>
   );
@@ -58,7 +48,7 @@ const styles = StyleSheet.create({
     // justifyContent: "center",
 
     // debug border
-    borderColor: "red",
+    // borderColor: "red",
     borderWidth: 1,
   },
 });
