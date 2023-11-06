553d07187a01f6a3222a759ce50097971b085104
jleesCY <joshualees@cedarville.edu>
2023-11-06 11:04:29 -0500

    Settings Page, SVGs -> JSX, and fonts

--- App.js
+++ App.js
@@ -2,22 +2,36 @@ import * as React from "react";
 import { StatusBar, StyleSheet } from "react-native";
 import { NavigationContainer, DefaultTheme } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import * as Font from "expo-font";
 import ChatsView from "./components/ChatsView";
+import Settings from "./components/Settings"
 
 const Stack = createNativeStackNavigator();
 
+const getFonts = () =>
+  Font.loadAsync({
+    scp: require("./assets/fonts/source-code-pro.ttf"),
+    ways: require("./assets/fonts/ways.ttf"),
+  });
+
 export default function App() {
+  
+  getFonts();
+
   return (
     <NavigationContainer>
       <StatusBar barStyle={"light-content"} />
-      <Stack.Navigator initialRouteName="ChatsView">
+      <Stack.Navigator initialRouteName="Settings">
         <Stack.Screen
           name="ChatsView"
           component={ChatsView}
           options={headerOptions.ChatsViewHeader}
         />
         {/* <Stack.Screen name="Profile" component={Profile} /> */}
-        {/* <Stack.Screen name="Settings" component={Settings} /> */}
+        {<Stack.Screen 
+          name="Settings" 
+          component={Settings} 
+          options={headerOptions.SettingsHeader}/>}
       </Stack.Navigator>
     </NavigationContainer>
   );
@@ -34,5 +48,12 @@ const headerOptions = {
     },
     headerTransparent: true,
     headerTitleStyle: { color: "#FFF" },
+  },
+  SettingsHeader: {
+    title: "Settings",
+    headerLargeTitle: false,
+    headerLargeTitleShadowVisible: false,
+    headerTransparent: true,
+    headerTitleStyle: { color: "transparent" },
   },
 };
---
+++ assets/images/jsx/NotificationsSVG.js
@@ -0,0 +1,13 @@
+import Svg, { G, Path } from 'react-native-svg'
+
+const NotificationsSVG = (props) => (
+<Svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 64 64" enable-background="new 0 0 64 64">
+<G>
+	<Path fill="#FFFFFF" d="M56,44c-1.832,0-4-2.168-4-4V20C52,8.973,43.027,0,32,0S12,8.973,12,20v20c0,1.793-2.207,4-4,4
+		c-2.211,0-4,1.789-4,4s1.789,4,4,4h48c2.211,0,4-1.789,4-4S58.211,44,56,44z"/>
+	<Path fill="#FFFFFF" d="M32,64c4.418,0,8-3.582,8-8H24C24,60.418,27.582,64,32,64z"/>
+</G>
+</Svg>
+)
+
+export default NotificationsSVG;
