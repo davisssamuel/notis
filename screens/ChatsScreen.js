71284b535478defcb402d23008e75e8489dd293c
Joshua Lees <82112905+jleesCY@users.noreply.github.com>
2024-02-10 20:47:49 -0500

    chats change

--- screens/ChatsScreen.js
+++ screens/ChatsScreen.js
@@ -1,7 +1,6 @@
 import { useNavigation } from "@react-navigation/native";
 import { SafeAreaView, FlatList, Pressable, Image, Text } from "react-native";
 import Chat from "../components/Chat";
-import AsyncStorage from "@react-native-async-storage/async-storage";
 
 // importing temp chats data
 import jsonData from "../data/chats.json";
@@ -9,7 +8,6 @@ import { useLayoutEffect } from "react";
 const chats = jsonData;
 
 export default async function ChatsScreen() {
-  //console.log(await AsyncStorage.getItem('privateKey'));
   return (
     <SafeAreaView style={{ flex: 1 }}>
       <FlatList
